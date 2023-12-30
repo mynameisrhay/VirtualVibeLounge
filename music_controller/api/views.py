a221ee9e0433034b07ebbe5b0ac3a08bef4d6f23
Raymond Bautista <mynameisrhay@gmail.com>
2023-12-30 21:40:58 +0800

    Created Serializers. Will create frontend after this.

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializers
 
 
-def main(request):
-    return HttpResponse("Hello World!")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializers
