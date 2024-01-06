3b777bf055fda8ec78718cb10468dda67f36f874
Raymond Bautista <mynameisrhay@gmail.com>
2024-01-06 16:25:41 +0800

    Polished tailwind installation.

--- music_controller/frontend/src/components/Message.tsx
+++ music_controller/frontend/src/components/Message.tsx
@@ -1,5 +1,10 @@
 function Intro() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1>Hello World! (Orig)</h1>
+      <h1 className="text-3xl font-bold underline">Hello world! (Tailwind)</h1>
+    </div>
+  );
 }
 
 export default Intro;
